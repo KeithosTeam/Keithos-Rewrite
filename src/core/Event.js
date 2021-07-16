f3ad8e954b0c089bb9b2107a183d224363f290c0
Cold <72723052+Colderry@users.noreply.github.com>
2021-07-16 21:10:07 +0600

    Event class

---
+++ src/core/Event.js
@@ -0,0 +1,21 @@
+const Client = require("../structures/Client");
+
+module.exports = class Event {
+    /**
+     * The discord.js client
+     * @param {Client} client 
+     */
+    constructor(client, option = {
+        name: "",
+        once: false
+    }) {
+        
+        this.client = client;
+        this.name = option.name || null;
+        this.once = option.once || false;
+    };
+
+    async run (...args) {
+        return;
+    };
+};
