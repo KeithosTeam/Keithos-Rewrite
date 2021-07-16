5be17275edf1db8a1cd67d66d6462bb64fbffaa9
Cold <72723052+Colderry@users.noreply.github.com>
2021-07-16 21:10:32 +0600

    ready

---
+++ src/core/events/client/ready.js
@@ -0,0 +1,26 @@
+const Event = require("../../Event");
+
+module.exports = class ready extends Event {
+    constructor(client) {
+        super(client, {
+            name: "ready",
+            once: true
+        });
+    };
+
+    async run() {
+
+        console.log(this.client.user.tag + " is online");
+
+        const activityArray = {
+            1: this.client.users.cache.size + " Users",
+            2: this.client.guilds.cache.size + " Guilds",
+            3: this.client.commands.size + " Commands"
+        };
+
+        setInterval(() => {
+            const activity = activityArray[Math.floor(Math.random() * 3)];
+            this.client.user.setActivity(activity, { type: "WATCHING" });
+        }, 16000);
+    };
+};
