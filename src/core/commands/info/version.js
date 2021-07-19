e02c1fdf0ed9020bd72dea90474a86e85705f515
Cold <72723052+Colderry@users.noreply.github.com>
2021-07-19 22:00:50 +0600

    version

---
+++ src/core/commands/info/version.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+const Command = require("../../Command");
+
+
+module.exports = class version extends Command {
+    constructor(client) {
+        super(client, {
+            name: "version",
+            description: "Display current version of Keithos",
+            aliases: ["ver"],
+            cooldown: 5,
+            type: client.types.INFO,
+            toggleCooldown: true
+        });
+    }; 
+
+    async run (message, args) {
+
+        const embed = new MessageEmbed()
+            .setTitle(`${this.client.user.username}`)
+            .setColor(this.client.config.embed.color)
+            .setThumbnail(this.client.user.displayAvatarURL())
+            .addField("Version", `\`${this.client.version}\``)
+            .setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true}));
+
+        return message.channel.send({ embeds: [embed]});
+    };
+};
