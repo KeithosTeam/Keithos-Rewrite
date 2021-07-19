cc9dddd559fcc82f5f7035ff25308d080932440b
Cold <72723052+Colderry@users.noreply.github.com>
2021-07-19 19:17:26 +0600

    log deleted message

---
+++ src/core/events/server/messageDelete.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+const Schema = require("../../../models/config");
+const Event = require("../../Event");
+
+
+module.exports = class MessageDelete extends Event {
+    constructor(client) {
+        super(client, {
+            name: "messageDelete"
+        });
+    };  
+
+    async run (message) {
+
+        const embed = new MessageEmbed()
+            .setTitle("Message Deleted")
+            .setColor("RED")
+            .setDescription(message.content)
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true}))
+            .addField("Author", `${message.author.tag} (${message.author.id})`)
+            .addField("Channel", `${message.channel}`)
+            .setTimestamp(Date.now());
+
+        Schema.findOne({ _id: message.guild.id }, async (e, data) => {
+            if (!data || data.messageLog === null) {
+                return;
+            } else {
+                return message.guild.channels.cache.get(data.messageLog).send({ embeds: [embed]}).catch(e => { return; });
+            };
+        });
+    };
+};
