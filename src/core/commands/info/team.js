323f56ca7e8736c310c52cf34dc2545dd31a50d2
Cold <72723052+Colderry@users.noreply.github.com>
2021-07-19 21:51:29 +0600

    Meet our team yes

---
+++ src/core/commands/info/team.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+const Command = require("../../Command");
+
+
+module.exports = class credit extends Command {
+    constructor(client) {
+        super(client, {
+            name: "team",
+            description: "Meet our team members and contributors",
+            aliases: ["team"],
+            cooldown: 8, 
+            toggleCooldown: true,
+            type: client.types.INFO
+        });
+    };
+
+    async run (message, args) {
+
+        const team = new MessageEmbed()
+            .setTitle("Meet our Team")
+            .setColor("AQUA")
+            .setThumbnail(this.client.user.displayAvatarURL())
+            .addField("Developers", "1. [MCorange](https://github.com/MCorange99) - Lead Developer\n\n2. [Colderry](https://github.com/Colderry) - Co Lead Developer")
+            .addField("Contributors", "None");
+
+        return message.channel.send({ embeds: [team]});
+    };
+};
