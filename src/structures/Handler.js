a4c74e234fe6718d9ad10f50459ce0dc121dd7de
Cold <72723052+Colderry@users.noreply.github.com>
2021-07-19 17:30:19 +0600

    loadCommands

--- src/structures/Handler.js
+++ src/structures/Handler.js
@@ -1,4 +1,5 @@
 const Event = require("../core/Event");
+const Command = require("../core/Command");
 const Client = require("./Client");
 
 module.exports = class Handler {
@@ -13,6 +14,33 @@ module.exports = class Handler {
         this.glob = require("glob");
 
     };
+
+    /**
+     * The path to the events folder
+     * @param {string} loc 
+     */
+    loadCommands (loc) {
+
+        const commands = this.glob.sync(loc);
+
+        for (const cmd of commands) {
+
+            const command = new (require(this.path.resolve(cmd)))(this.client);
+
+            if (!command instanceof(Command) || !command.name) {
+                return;
+            };
+
+            this.client.commands.set(command.name, command);
+
+            if (command.aliases && Array.isArray(command.aliases)) {
+
+                command.aliases.forEach(alias => this.client.aliases.set(alias, command));
+
+            };
+        };
+    };
+
     /**
      * The path to the events folder
      * @param {string} loc 
@@ -28,8 +56,12 @@ module.exports = class Handler {
             if (!event instanceof(Event)) {
                 return;
             };
-
-            this.client.on(event.name, (...args) => event.run(...args));
+            
+            if (event.once) {
+                this.client.once(event.name, (...args) => event.run(...args));
+            } else {
+                this.client.on(event.name, (...args) => event.run(...args));
+            };
         };
     };
 };
