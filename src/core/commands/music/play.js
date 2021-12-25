c1325fbd2e55f1f3d19f639751d3cb41c8a1e40d
MCorange <gvidasjuknevicius2@gmail.com>
2021-12-25 21:51:26 +0200

    music command

---
+++ src/core/commands/music/play.js
@@ -0,0 +1,61 @@
+const { MessageEmbed } = require('discord.js');
+const Command = require('../../Command');
+const DisTube = require('distube')
+
+
+module.exports = class Vote extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'play',
+			description: 'The music play command',
+			aliases: ['p','pause', 'hold', 'resume', 'unpause', 'stop', 's', 'disconnect','leave' , 'skip', 'queue', 'q'],
+			type: client.types.MUSIC
+		});
+	}
+
+	async run(message, args) {
+
+        Schema.findOne({ _id: message.guild.id }, async (e, data) => {
+        const prefix = data.prefix
+        const x = message.content.replace(prefix, "").split(" ")[0]
+        const distube = new DisTube.default(message.client)
+
+        if (['play', 'p'].includes(x)){
+            distube.play(message, args.join(' '))
+
+            distube.on("playSong", (queue, song) => queue.textChannel.send(
+                    `${this.emoji.success}Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`
+                ));
+        } else if (['stop', 's', "disconnect", "leave"].includes(x)) {
+            const queue = distube.getQueue(message)
+            if (!queue) return message.channel.send(`${this.emoji.fail} | There is nothing in the queue right now!`)
+            queue.stop()
+            message.channel.send(`${this.emoji.success} | Stopped!`)
+        } else if (['skip'].includes(x)){
+            const queue = distube.getQueue(message)
+            if (!queue) return message.channel.send(`${this.emoji.error} | There is nothing in the queue right now!`)
+            const song = queue.skip()
+            message.channel.send(`${this.emoji.success} | Skipped! Now playing:\n${song.name}`)
+        } else if (['pause', 'hold'].includes(x)){
+            const queue = client.distube.getQueue(message)
+            if (!queue) return message.channel.send(`${this.emoji.error} | There is nothing in the queue right now!`)
+            if (queue.pause) {
+                queue.resume()
+                return message.channel.send("Resumed the song for you :)")
+            }
+            queue.pause()
+            message.channel.send("Paused the song for you :P")
+        } else if (["resume", "unpause"].includes(x)){
+            const queue = client.distube.getQueue(message)
+            if (!queue) return message.channel.send(`${this.emoji.error} | There is nothing in the queue right now!`)
+            queue.resume()
+            message.channel.send(this.emoji.success + " Resumed the song for you :P")
+        } else if (['queue', 'q'].includes(x)){
+            const queue = client.distube.getQueue(message)
+            if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing playing!`)
+            const q = queue.songs.map((song, i) => `${i === 0 ? "Playing:" : `${i}.`} ${song.name} - \`${song.formattedDuration}\``).join("\n")
+            message.channel.send(`${client.emotes.queue} | **Server Queue**\n${q}`)
+        }
+        });
+	}
+};
