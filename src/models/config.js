2fd2df7c8ac855c3ed547649679537da7cfa4fac
MCorange <gvidasjuknevicius2@gmail.com>
2021-12-30 19:40:55 +0200

    this is the most complex shit ive ever done

--- src/models/config.js
+++ src/models/config.js
@@ -6,17 +6,24 @@ module.exports = model(
 	new Schema({
 		_id: String,
 		prefix: { type: String, default: prefix },
+		ip: { type: String, default: null},
+
 		messageLog: { type: String, default: null},
 		joinLog: { type: String, default: null},
 		leaveLog: { type: String, default: null},
 		nickLog: { type: String, default: null},
 		roleLog: { type: String, default: null},
+		modLog: { type: String, default: null},
+
 		welcomeLog: { type: String, default: null},
 		farewellLog: { type: String, default: null},
 		joinMsg: { type: String, default: null},
 		leaveMsg: { type: String, default: null},
-		ip: { type: String, default: null},
-		modLog: { type: String, default: null}
+		
+		modRole: { type: String, default: null},
+		adminRole: { type: String, default: null},
+		autoRole: { type: String, default: null},
+		
 	}, {
 		versionKey: false
 	})
